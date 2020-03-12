257089aacc9d90545c23109387aa69542a478b2b
simon-kingston <simonkingston@eventmapsolutions.com>
2020-03-12 11:40:35 +0000

    initial commit - Spring Initializr generated spring boot project

---
+++ src/main/java/com/ni/town/NITownsTSP/NiTownsTspApplication.java
@@ -0,0 +1,13 @@
+package com.ni.town.NITownsTSP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiTownsTspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiTownsTspApplication.class, args);
+	}
+
+}
